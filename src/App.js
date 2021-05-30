2fcad22eb2aca6430c43b134ff9080fcb87b7974
Pasha-Man <57852014+Pasha-Man@users.noreply.github.com>
2021-05-30 15:22:20 +0500

    FYP files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {Route} from "react-router-dom";
+import Home from "./home";
+import Service from "./service";
+import Settings from "./settings";
+import Signin from "./Login";
+import Signup from "./Signup";
+function App(){
+  return (
+    <switch>
+      <Route path="/Signup" component={Signup} default />
+      <Route path="/" component={Signin} exact />
+      <Route path="/dashboard" component={Home} exact />
+      <Route path="/service" component={Service} />
+      <Route path="/settings" component={Settings} />
+    </switch>
+  );
+}
+export default App;
